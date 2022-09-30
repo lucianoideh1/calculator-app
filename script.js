7429e9743293c3e87e04b8198f2128c0f4ff2aa6
Luciano Vazquez <vazquezluciano5@gmail.com>
2022-09-30 16:13:10 -0300

    added javascript and jquery

---
+++ script.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    $("#one").click(function(){
+        document.forms.display.value += 1;
+    })
+    $("#two").click(function(){
+        document.forms.display.value += 2;
+    })
+    $("#three").click(function(){
+        document.forms.display.value += 3;
+    })
+    $("#four").click(function(){
+        document.forms.display.value += 4;
+    })
+    $("#five").click(function(){
+        document.forms.display.value += 5;
+    })
+    $("#six").click(function(){
+        document.forms.display.value += 6;
+    })
+    $("#seven").click(function(){
+        document.forms.display.value += 7;
+    })
+    $("#eight").click(function(){
+        document.forms.display.value += 8;
+    })
+    $("#nine").click(function(){
+        document.forms.display.value += 9;
+    })
+    $("#divide").click(function(){
+        document.forms.display.value += "/";
+    })
+    $("#multiply").click(function(){
+        document.forms.display.value += "*";
+    })
+    $("#substract").click(function(){
+        document.forms.display.value += "-";
+    })
+    $("#add").click(function(){
+        document.forms.display.value += "+";
+    })
+    $("#dot").click(function(){
+        document.forms.display.value += ".";
+    })
+    $("#zero").click(function(){
+        document.forms.display.value += 0;
+    })
+})
